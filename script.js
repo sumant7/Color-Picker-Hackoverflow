846bb387c19a71a0bc683765eb9ab6b58d31f6ae
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-01 20:34:06 +0530

    Home page done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ ins.onclick = function(){
     const display= document.querySelector('.instruction')
     if(display.innerHTML=="")
     {
-        display.innerHTML= "You will be given a random RGB value on top.RGB stands for Red Blur Green. There will be 3 colored cards in easy mode and 6 colored cards in hard mode. One of the colored cards will be of the correct RGB value as displayed on the top. You have to choose between the given options."
+        display.innerHTML= "You will be given a random RGB value on top. RGB stands for Red Blur Green. There will be 3 colored cards in easy mode and 6 colored cards in hard mode. One of the colored cards will be of the correct RGB value as displayed on the top. You have to choose between the given options. You choose the correct option you win."
     }
     else{
         display.innerHTML=""
