62fde1943dcd2878970dc6ffe2e85715e36f9d74
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-02 19:50:51 +0530

    Hard Mode Done

--- Hard/script.js
+++ Hard/script.js
@@ -9,6 +9,7 @@ let correct
 let color
 let win =0
 
+
 generate.onclick = function(){   
     win=0
     up.style.backgroundColor= "rgb(70, 140, 255)"
@@ -17,15 +18,16 @@ generate.onclick = function(){
     generate.innerText= "GENERATE AGAIN"
     color= randcolor()
     code.innerText= `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-    correct = Math.floor(Math.random()*3) + 1   //decides which of the three options will have correct color 
-    for (let i = 0; i < 3; i++) {   //since there are three options
-        if(i==(correct-1))   //correct option will display the correct code
+    correct = Math.floor(Math.random()*6) + 1  //decides which of the six options will have correct color 
+    console.log(correct)
+    for (let i = 1; i <= 6; i++) {   //since there are three options
+        if(i==correct)   //correct option will display the correct code
         {
             document.getElementById(`box${correct}`).style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
             continue
         }
         option = randcolor()  //other options display random code
-        document.getElementById(`box${i+1}`).style.backgroundColor = `rgb(${option[0]}, ${option[1]}, ${option[2]})`
+        document.getElementById(`box${i}`).style.backgroundColor = `rgb(${option[0]}, ${option[1]}, ${option[2]})`
     }
 
 }
@@ -42,50 +44,24 @@ function randcolor(){  //returns an array with 3 elemets as color code
 let box1 = document.getElementById('box1')
 let box2 = document.getElementById('box2')
 let box3 = document.getElementById('box3')
-
-
-
-//optimised approach don't know why not working
-// box1.addEventListener("click", check(1));
-// box2.addEventListener("click", check(2));
-// box3.addEventListener("click", check(3));
-
-
-// function check(p){
-//     console.log(69)
-//     if(correct==p)
-//     {
-//         message.innerText = "YOU WIN!"
-//         win=1
-//         box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-//         box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-//         box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-//         up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
-//     }
-//     else
-//     {
-//         if(win!=1)
-//         {
-//             message.innerText = "Try Again"
-//             document.getElementById(`box${p}`).style.backgroundColor = "rgb(41, 38, 38)"
-//         }
-//     }
-// }
-
-
-
+let box4 = document.getElementById('box4')
+let box5 = document.getElementById('box5')
+let box6 = document.getElementById('box6')
 
 
 
 //hardcode
 box1.onclick = function(){
-    console.log(69)
     if(correct==1)
     {
         message.innerText = "YOU WIN!"
         win=1
+        box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
     }
     else
@@ -103,7 +79,11 @@ box2.onclick = function(){
         message.innerText = "YOU WIN!"
         win=1
         box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
     }
     else
@@ -120,8 +100,12 @@ box3.onclick = function(){
     {
         message.innerText = "YOU WIN!"
         win=1
-        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
         up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
     }
     else
@@ -130,6 +114,72 @@ box3.onclick = function(){
         {
             message.innerText = "Try Again"
             box3.style.backgroundColor = "rgb(41, 38, 38)"
+        }
+    }
+}
+box4.onclick = function(){
+    if(correct==4)
+    {
+        message.innerText = "YOU WIN!"
+        win=1
+        box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+    }
+    else
+    {
+        if(win!=1)
+        {
+            message.innerText = "Try Again"
+            box4.style.backgroundColor = "rgb(41, 38, 38)"
+        }
+    }
+}
+box5.onclick = function(){
+    if(correct==5)
+    {
+        message.innerText = "YOU WIN!"
+        win=1
+        box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+    }
+    else
+    {
+        if(win!=1)
+        {
+            message.innerText = "Try Again"
+            box5.style.backgroundColor = "rgb(41, 38, 38)"
+        }
+    }
+}
+box6.onclick = function(){
+    if(correct==6)
+    {
+        message.innerText = "YOU WIN!"
+        win=1
+        box1.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box2.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box3.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box4.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box5.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        box6.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        up.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+    }
+    else
+    {
+        if(win!=1)
+        {
+            message.innerText = "Try Again"
+            box6.style.backgroundColor = "rgb(41, 38, 38)"
         }
     }
 }
